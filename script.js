0fc4125e48f97f957e29ce0de9d5f1deab5c8d41
tmorgan1987 <tmorgan1987@outlook.com>
2022-09-25 23:57:14 -0400

    added variable for current time

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 var time = moment().format("dddd MMM Do");
 $("#currentDay").text(time);
+
+var currentTime = moment().format('h:mm:ss a');
+console.log(currentTime);
+
+function nineAM() {
+	if (currentTime > 9) {
+
+	}
+}
